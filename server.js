abb1fdbb62bbff8dfe178a2eacbfd2e70d9b1f30
NyanLinHtet0 <harrishtet.nyanlinhtet@gmail.com>
2025-04-09 01:15:33 -0700

    added basic html webpage files and connect using java script

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html/index.html'))
+})
+
+app.get('/html/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html/about.html'))
+})
+
+app.get('/html/projects', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html/projects.html'))
+})
+
+app.get('/html/skills', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html/skills.html'))
+})
+
+app.get('/html/contacts', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html/contacts.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
