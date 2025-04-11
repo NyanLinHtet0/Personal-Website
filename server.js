2770f9bf1863f5c114605728ba5343718f132a90
NyanLinHtet0 <harrishtet.nyanlinhtet@gmail.com>
2025-04-11 12:22:30 -0700

    setted up html, js, and css folders and served  them successfully

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const app = express()
 const port = 3000
 
+app.use('/HTML', express.static(path.join(__dirname, 'html')));
+app.use('/CSS', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'html/index.html'))
 })
