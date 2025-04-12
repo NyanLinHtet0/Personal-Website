22cb3dd20e6ca2a573c0b64812bec24e67fd173c
Your Name <harrishtet.nyanlinhtet@gmail.com>
2025-04-12 14:37:37 -0700

    working on menu annimations half done

--- js/page_menu.js
+++ js/page_menu.js
@@ -1,1 +1,16 @@
 console.log("Hello World")
+
+const button_menu_toggle = document.getElementById("menu_button")
+
+button_menu_toggle.addEventListener("click", function() {
+    console.log("button clicked")
+    const menu = document.getElementById("page_menu_id");
+    if (menu) {
+        menu.style.transform = "translateX(350px)";
+    }
+    const menu_toggle = document.getElementById("menu_button");
+    if (menu_toggle) {
+        menu_toggle.style.transform = "translateX(-130px)";
+    }
+} );
+
