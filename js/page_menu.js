63514f6dc2b7b6573ca5d4d6a997eb228ff9691a
Your Name <harrishtet.nyanlinhtet@gmail.com>
2025-04-13 00:07:18 -0700

    fully made menu component of the website in css, js, and html with animations (mobile viewport development still needed)

--- js/page_menu.js
+++ js/page_menu.js
@@ -1,16 +1,32 @@
 console.log("Hello World")
 
-const button_menu_toggle = document.getElementById("menu_button")
+const button_menu_toggle_outside = document.getElementById("menu_button_outside_id")
+const button_menu_toggle_inside = document.getElementById("menu_button_inside_id")
 
-button_menu_toggle.addEventListener("click", function() {
+button_menu_toggle_outside.addEventListener("click", function() {
     console.log("button clicked")
-    const menu = document.getElementById("page_menu_id");
-    if (menu) {
-        menu.style.transform = "translateX(350px)";
+    const menu = document.getElementById("page_menu_id")
+    if (menu){
+        button_menu_toggle_outside.style.animation = "fadeOut .125s ease-in-out forwards";
+        setTimeout(() => {
+            button_menu_toggle_outside.style.display = "none";
+            menu.style.opacity = "0";
+            menu.style.display = "flex";
+            menu.style.animation = "fadeIn .125s ease-in-out both";
+        }, 125);
     }
-    const menu_toggle = document.getElementById("menu_button");
-    if (menu_toggle) {
-        menu_toggle.style.transform = "translateX(-130px)";
+} );
+
+button_menu_toggle_inside.addEventListener("click", function() {
+    console.log("button clicked")
+    const menu = document.getElementById("page_menu_id")
+    if (menu){
+        menu.style.animation = "fadeOut .125s ease-in-out forwards";
+        setTimeout(() => {
+            menu.style.display = "none";
+            button_menu_toggle_outside.style.display = "flex";
+            button_menu_toggle_outside.style.animation = "fadeIn .125s ease-in-out both";
+        }, 125);
     }
 } );
 
