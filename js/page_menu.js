dc54005984a780299cbc33bba122328512e4a126
Your Name <harrishtet.nyanlinhtet@gmail.com>
2025-04-13 00:42:12 -0700

    added body container that adjusts when the menu is opened or closed, so data can be dumped into the container later after mobile viewport is setup

--- js/page_menu.js
+++ js/page_menu.js
@@ -3,23 +3,24 @@ console.log("Hello World")
 const button_menu_toggle_outside = document.getElementById("menu_button_outside_id")
 const button_menu_toggle_inside = document.getElementById("menu_button_inside_id")
 
+
 button_menu_toggle_outside.addEventListener("click", function() {
     console.log("button clicked")
-    const menu = document.getElementById("page_menu_id")
+    const menu = document.getElementById("page_menu_id");
     if (menu){
         button_menu_toggle_outside.style.animation = "fadeOut .125s ease-in-out forwards";
         setTimeout(() => {
             button_menu_toggle_outside.style.display = "none";
             menu.style.opacity = "0";
             menu.style.display = "flex";
-            menu.style.animation = "fadeIn .125s ease-in-out both";
+            menu.style.animation = "fadeIn .125s ease-in-out both";     
         }, 125);
     }
 } );
 
 button_menu_toggle_inside.addEventListener("click", function() {
-    console.log("button clicked")
-    const menu = document.getElementById("page_menu_id")
+    console.log("button clicked");
+    const menu = document.getElementById("page_menu_id");
     if (menu){
         menu.style.animation = "fadeOut .125s ease-in-out forwards";
         setTimeout(() => {
